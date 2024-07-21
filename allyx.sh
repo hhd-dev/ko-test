0d2a24615eae7aa95f26d4a57633d4d56e367f47
antheas <git@antheas.dev>
2024-07-21 17:48:07 +0300

    update ally script

--- allyx.sh
+++ allyx.sh
@@ -6,21 +6,27 @@ LOCAL_DIR="/tmp/ko-git"
 set -e
 
 # Cleaning up download dir
+echo "# Downloading data to '$LOCAL_DIR'"
 rm -rf $LOCAL_DIR
 mkdir -p $LOCAL_DIR
 git clone $UPSTREAM_URL $LOCAL_DIR
 
 REV=$(git -C $LOCAL_DIR rev-parse --short HEAD)
-echo "Using git version: $REV"
+echo
+echo "# Using git version: $REV"
 
 # Removing old modules
+echo
+echo "# Removing old modules"
 for module in "${MODULES[@]}"; do
-    echo "Removing module '$module'"
+    echo "- Removing module: '$module'"
     sudo rmmod $module || true
 done
 
 # Applying new modules
+echo
+echo "# Applying new modules"
 for module in "${MODULES[@]}"; do
-    echo "Applying module '$module'"
+    echo "- Applying module: '$module'"
     sudo insmod $LOCAL_DIR/$module.ko
 done
