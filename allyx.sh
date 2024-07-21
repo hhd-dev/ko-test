0c645b3211bfd2ba04f61dbe5c6b7f5de0df499a
antheas <git@antheas.dev>
2024-07-21 17:44:44 +0300

    update sh script

--- allyx.sh
+++ allyx.sh
@@ -2,12 +2,16 @@
 
 MODULES=("asus-wmi")
 UPSTREAM_URL="https://github.com/hhd-dev/ko-test"
+LOCAL_DIR="/tmp/ko-git"
 set -e
 
 # Cleaning up download dir
-rm -rf /tmp/ko-git
-mkdir -p /tmp/ko-git
-git clone $UPSTREAM_URL /tmp/ko-git
+rm -rf $LOCAL_DIR
+mkdir -p $LOCAL_DIR
+git clone $UPSTREAM_URL $LOCAL_DIR
+
+REV=$(git -C $LOCAL_DIR rev-parse --short HEAD)
+echo "Using git version: $REV"
 
 # Removing old modules
 for module in "${MODULES[@]}"; do
@@ -18,5 +22,5 @@ done
 # Applying new modules
 for module in "${MODULES[@]}"; do
     echo "Applying module '$module'"
-    sudo insmod /tmp/ko-git/$module.ko
+    sudo insmod $LOCAL_DIR/$module.ko
 done
