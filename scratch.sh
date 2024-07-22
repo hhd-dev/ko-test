a2a841616d9d39024b85604e8c24fc48c5b1cdc5
antheas <git@antheas.dev>
2024-07-22 19:23:15 +0300

    update module

--- scratch.sh
+++ scratch.sh
@@ -4,8 +4,8 @@
 curl https://raw.githubusercontent.com/hhd-dev/ko-test/master/patch.sh | sh
 
 # sleep
-echo `date '+%s' -d "+ 10 seconds"` | sudo tee /sys/class/rtc/rtc0/wakealarm; sudo systemctl suspend
+echo `date '+%s' -d "+ 15 seconds"` | sudo tee /sys/class/rtc/rtc0/wakealarm; sudo systemctl suspend
 
 # do both
 curl https://raw.githubusercontent.com/hhd-dev/ko-test/master/patch.sh | sh && \
-    echo `date '+%s' -d "+ 10 seconds"` | sudo tee /sys/class/rtc/rtc0/wakealarm; sudo systemctl suspend
+    echo `date '+%s' -d "+ 15 seconds"` | sudo tee /sys/class/rtc/rtc0/wakealarm; sudo systemctl suspend
