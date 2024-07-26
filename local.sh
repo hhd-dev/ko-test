8abb41a491974ed8bf859a8714633b48bfc25d2a
Antheas Kapenekakis <git@antheas.dev>
2024-07-27 01:27:36 +0300

    switch to sound drivers

--- local.sh
+++ local.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DISABLE_MODULES=("asus-nb-wmi" "asus-bios" "asus-wmi")
-CHANGED_MODULES=("asus-wmi")
-CHILD_MODULES=("asus-nb-wmi" "asus-bios")
-
 LOCAL_DIR="/tmp/ko-git"
+CHANGED_MODULES=$(find $LOCAL_DIR -name "*.ko")
+CHILD_MODULES=()
+DISABLE_MODULES=( "$CHANGED_MODULES[@]" "$CHILD_MODULES[@]" )
+
 set -e
 
 # Removing old modules
@@ -12,7 +12,7 @@ echo
 echo "# Removing old modules"
 for module in "${DISABLE_MODULES[@]}"; do
     echo "- Removing module: '$module'"
-    sudo rmmod $module || true
+    sudo rmmod $(basename $module) || true
 done
 
 # Applying new modules
@@ -20,7 +20,7 @@ echo
 echo "# Applying new modules"
 for module in "${CHANGED_MODULES[@]}"; do
     echo "- Applying patched module: '$module'"
-    sudo insmod $LOCAL_DIR/$module.ko
+    sudo insmod $LOCAL_DIR/$module
 done
 
 # Applying new modules
@@ -28,5 +28,5 @@ echo
 echo "# Reloading dependent modules"
 for module in "${CHILD_MODULES[@]}"; do
     echo "- Re-enabling module: '$module'"
-    sudo modprobe $module
+    sudo modprobe $(basename $module)
 done
