dcea98f139b7fe78c00c4e285e078d93208f7b89
Antheas Kapenekakis <git@antheas.dev>
2024-07-27 19:30:20 +0300

    update local.sh to be generic

--- local.sh
+++ local.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# Find changed modules
 LOCAL_DIR="/tmp/ko-git"
-CHANGED_MODULES=$(find $LOCAL_DIR -name "*.ko")
-CHILD_MODULES=()
-DISABLE_MODULES=( "$CHANGED_MODULES[@]" "$CHILD_MODULES[@]" )
+CHANGED_MODULES=($(find $LOCAL_DIR -name "*.ko"))
+if [ -z "$CHANGED_MODULES" ]; then
+    echo "No modules found in '$LOCAL_DIR'"
+    exit 1
+fi
+CHANGED_MODULE_NAMES=($(basename -a ${CHANGED_MODULES[@]%.ko}))
+
+# In case those modules have dependents, handle them manually as well
+# CHILD_MODULES=("asus-wmi")
+if [ -z "$CHILD_MODULES" ]; then
+    DISABLE_MODULES=("${CHANGED_MODULE_NAMES[@]}")
+else
+    DISABLE_MODULES=( "${CHANGED_MODULE_NAMES[@]}" "${CHILD_MODULES[@]}" )
+fi
 
 set -e
+
+#
+# Begin swap
+#
 
 # Removing old modules
 echo
@@ -20,8 +36,12 @@ echo
 echo "# Applying new modules"
 for module in "${CHANGED_MODULES[@]}"; do
     echo "- Applying patched module: '$module'"
-    sudo insmod $LOCAL_DIR/$module
+    sudo insmod $module
 done
+
+if [ -z "$CHILD_MODULES" ]; then
+    exit 0
+fi
 
 # Applying new modules
 echo
